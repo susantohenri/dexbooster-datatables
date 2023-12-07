8b91ca8d582293e29e26b1a308cf54ed41ca4cc7
Henri Susanto <henry.dinus@gmail.com>
2023-12-07 21:26:31 +0700

    last working

--- dexbooster-datatables.php
+++ dexbooster-datatables.php
@@ -11,7 +11,7 @@
  * @wordpress-plugin
  * Plugin Name: Dex Booster Datatables
  * Plugin URI:  https://github.com/susantohenri/dexbooster-datatables
- * Description: Datatables plugin for dexbooster.io to speed up page load by serving large JSON through PHP backend <strong>sample usage:</strong> [dexbooster-datatables json-url="https://ffxkccymzr.a.pinggy.online/data_arbitrum"]
+ * Description: Update current wpdatatable: set input data source type to "SQL query", check "Enable server-side processing", and SQL Query to "SELECT * FROM `arbitrum` WHERE 'https://henri.xsanisty.com/data_arbitrum.json' = 'https://henri.xsanisty.com/data_arbitrum.json'" (change URL with json source)
  * Version:     1.0.0
  * Author:      Henri Susanto
  * Author URI:  https://github.com/susantohenri/
@@ -20,91 +20,119 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-add_shortcode('dexbooster-datatables', function ($attributes) {
-    if (!isset($attributes['json-url'])) return '<strong>invalid short-code usage, use:</strong> [dexbooster-datatables json-url="https://ffxkccymzr.a.pinggy.online/data_arbitrum"]';
+define('DEX_BOOSTER_DATATABLES_CRON_INTERVAL_MINUTES', 10);
 
-    wp_register_style('datatables', 'https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css');
-    wp_enqueue_style('datatables');
+add_action('wp_ajax_get_wdtable', 'dexbooster_datatables_ajax', 1);
+add_action('wp_ajax_nopriv_get_wdtable', 'dexbooster_datatables_ajax', 1);
+function dexbooster_datatables_ajax()
+{
+    $result = [
+        'draw' => intval($_POST['draw']),
+        'recordsTotal' => 0,
+        'recordsFiltered' => 0,
+        'data' => []
+    ];
 
-    wp_register_script('datatables', 'https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js', ['jquery']);
-    wp_enqueue_script('datatables');
+    $columns = $_POST['columns'];
+    $dir = $_POST['order'][0]['dir'];
+    $col = $columns[$_POST['order'][0]['column']]['name'];
+    $search = urldecode($_POST['search']['value']);
+    $wpdt_id = $_GET['table_id'];
 
-    wp_enqueue_script('dexbooster-datatables', plugin_dir_url(__FILE__) . 'dexbooster-datatables.js?token=' . time(), null, null, true);
-    wp_localize_script(
-        'dexbooster-datatables',
-        'dexbooster_datatables',
-        array(
-            'json_parser_url' => site_url("wp-json/dexbooster-datatables/v1/parse-json?&cache-breaker=" . time()),
-        )
-    );
+    $json = dexbooster_datatables_get_uptodate_json_data($wpdt_id);
+    $data = ['data' => $json['data']];
 
-    return "
-        <table class='dexbooster-datatables' json-url='{$attributes['json-url']}'>
-            <thead>
-                <tr>
-                    <th>PAIR</th>
-                    <th>TIER</th>
-                    <th>APR</th>
-                    <th>PRICE</th>
-                    <th>TVL</th>
-                    <th>DEX</th>
-                    <th>INFO</th>
-                </tr>
-            </thead>
-            <tbody>
-            </tbody>
-        </table>
-    ";
-});
+    $rows = [];
+    foreach ($data['data'] as $row) {
+    if (
+			(empty($search) || 
+			(!empty($search) && (stripos($row['Pair'], $search) !== false || stripos($row['Address'], $search) !== false)))
+		) {
+			$rows[] = $row;
+		}
+	}
 
-add_action('rest_api_init', function () {
-    register_rest_route('dexbooster-datatables/v1', '/parse-json', array(
-        'methods' => 'POST',
-        'permission_callback' => '__return_true',
-        'callback' => function () {
-            $contents = file_get_contents($_POST['source'], 0, stream_context_create(array(
-                'ssl' => array(
-                    'verify_peer' => false,
-                    'verify_peer_name' => false,
-                ),
-                'http' => array(
-                    'timeout' => 30
-                )
-            )));
+    uasort($rows, fn ($a, $b) => ($dir === 'asc') ? $a[$col] <=> $b[$col] : $b[$col] <=> $a[$col]);
+    $data_slice = array_slice($rows, $_POST['start'], $_POST['length']);
 
-            $json = json_decode($contents, true);
-            $json = array_map(function ($obj) {
-                $obj = array_filter($obj, function ($value, $attr) {
-                    return in_array($attr, ['Pair', 'Tier', 'apr_mensual', 'Price_USD', 'TVL 2', 'Dex_image', 'FDV']);
-                }, ARRAY_FILTER_USE_BOTH);
-                return array_values($obj);
-            }, $json);
+    $header_positions = [];
+    global $wpdb;
+    foreach ($wpdb->get_results("SELECT orig_header, pos FROM {$wpdb->prefix}wpdatatables_columns WHERE table_id={$wpdt_id}") as $header) {
+        $header_positions[$header->pos] = $header->orig_header;
+    }
 
-            $data = ['data' => $json];
-
-            $columns = $_POST['columns'];
-            $dir = $_POST['order'][0]['dir'];
-            $col = $columns[$_POST['order'][0]['column']]['name'];
-
-            $search = urldecode($_POST['search']['value']);
-            $rows = [];
-            foreach ($data['data'] as $row) {
-                if (
-                    (empty($search) || (!empty($search) && (stripos($row['key'], $search) !== false || stripos($row['id'], $search) !== false)))
-                ) {
-                    $rows[] = $row;
-                }
+    foreach ($data_slice as $index => $value) {
+        $reordereds = [];
+        $address = '';
+        foreach ($value as $key => $obj) {
+            if ('Dex_image' == $key) $value[$key] = "<img src='{$obj}'>";
+            else if ('Address' === $key) $address = $obj;
+            else $value[$key] = strval($obj);
+            foreach ($header_positions as $pos => $orig_header) {
+                $reordereds[(int)$pos] = $value[$orig_header];
             }
-
-            uasort($rows, fn ($a, $b) => ($dir === 'asc') ? $a[$col] <=> $b[$col] : $b[$col] <=> $a[$col]);
-            $data_slice = array_slice($rows, $_POST['start'], $_POST['length']);
-
-            return [
-                'draw' => intval($_POST['draw']),
-                'recordsTotal' => count($data['data']),
-                'recordsFiltered' => count($rows),
-                'data' => $data_slice
-            ];
         }
-    ));
+
+        $data_slice[$index] = $reordereds;
+        $pool_url = site_url("pool?blockchain={$json['crypto']}&wpdtid={$wpdt_id}&address={$address}");
+        $data_slice[$index][count($reordereds) - 1] = "<form class='wdt_md_form' method='post' target='_blank' action='{$pool_url}'><input class='wdt_md_hidden_data' type='hidden' name='wdt_details_data' value=''><input class='master_detail_column_btn my-button' type='submit' value='🚀'></form>";
+    }
+
+    $result = [
+        'draw' => intval($_POST['draw']),
+        'recordsTotal' => strval(count($data['data'])),
+        'recordsFiltered' => strval(count($rows)),
+        'data' => $data_slice
+    ];
+
+    echo json_encode($result);
+    wp_die();
+}
+
+add_action('init', function () {
+    if (isset($_GET['wpdtid']) && isset($_GET['address'])) {
+        $params = [
+            'crypto' => $_GET['crypto'],
+            'wpdtid' => $_GET['wpdtid'],
+            'address' => $_GET['address'],
+        ];
+        $json = dexbooster_datatables_get_uptodate_json_data($params['wpdtid']);
+        $rows = array_values(array_filter($json['data'], function ($record) use ($params) {
+            return $record['Address'] == $params['address'];
+        }));
+        $rows[0]['wdt_md_id_table'] = $params['wpdtid'];
+        $rows[0]['Dex_image'] = "<img src='{$rows[0]['Dex_image']}'>";
+        $rows = json_encode($rows[0]);
+        $rows = addslashes($rows);
+        $_POST['wdt_details_data'] = $rows;
+    }
 });
+
+function dexbooster_datatables_get_uptodate_json_data($wpdt_id)
+{
+    global $wpdb;
+    $source = $wpdb->get_var("SELECT SUBSTR(content, LOCATE('=', content)) FROM `{$wpdb->prefix}wpdatatables` WHERE id = {$wpdt_id}");
+    $source = str_replace('=', '', $source);
+    $source = str_replace("'", '', $source);
+    $source = trim($source);
+
+    $crypto_data = explode('/', $source);
+    $crypto_data = end($crypto_data);
+
+    $downloaded_json_file = plugin_dir_path(__FILE__) . "{$crypto_data}.json";
+    $last_downloaded = round(abs(time() - filemtime($downloaded_json_file)) / 60, 2);
+    if ($last_downloaded >= DEX_BOOSTER_DATATABLES_CRON_INTERVAL_MINUTES) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $source);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        $contents = curl_exec($ch);
+        curl_close($ch);
+        file_put_contents($downloaded_json_file, $contents);
+    }
+    return [
+        'crypto' => str_replace('data_', '', $crypto_data),
+        'data' => json_decode(file_get_contents($downloaded_json_file), true)
+    ];
+}
